7a77600acdc41111d152a177ea5479ec9e7febfc
alefsloan <alef_sloan@hotmail.com>
2023-06-26 14:28:28 -0300

    feat: create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+import { HeaderContent } from "./style";
+
+// import headerCover from '../../assets/header_cover.svg';
+
+export function Header() {
+  return (
+    <HeaderContent/>
+  )
+}
---
+++ src/components/Header/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const HeaderContent = styled.header`
+  width: 100%;
+  height: 296px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-image: url('./src/assets/cover.png');
+`
